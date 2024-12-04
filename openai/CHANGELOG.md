da7da2f74f422bb4e9bb0cefeaa55ced6d87ade9
iuroc <m@iuroc.com>
2024-12-05 04:50:13 +0800

    ...

---
+++ openai/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [v1.0.0] 2024.12.5
+
+OpenAI 请求工具库，基本保留原 npm 库 TypeScript 定义。
---
+++ openai/src/main/ets/types.ts
@@ -0,0 +1,3 @@
+import { ChatCompletionMessageParam } from './openai@4.73.1/resources/chat/completions'
+
+export type ChatCompletionMessageParam_role = ChatCompletionMessageParam['role']
